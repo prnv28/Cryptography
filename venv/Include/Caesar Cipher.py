6574fd110b93c94fd71fb2b14043ec7cc20b9f9b
Pranav Gajera <pranavgajera1998@gmail.com>
2019-01-06 23:35:50 +0530

    Initial commit

---
+++ venv/Include/Caesar Cipher.py
@@ -0,0 +1,22 @@
+# letters to ints and the inverse
+L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
+I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+key = 3
+plaintext = "YOU WILL NEVER KNOW."
+
+# encipher
+ciphertext = ""
+for c in plaintext.upper():
+    if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
+    else: ciphertext += c
+
+# decipher
+plaintext2 = ""
+for c in ciphertext.upper():
+    if c.isalpha(): plaintext2 += I2L[ (L2I[c] - key)%26 ]
+    else: plaintext2 += c
+
+print ("Original Text : {}".format(plaintext))
+print ("Encrypted Text : {}".format(ciphertext))
+print ("Encrypted Text : {}".format(plaintext2))
